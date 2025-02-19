13082dacf8ab45036cd163d575e83956ac1c82bf
Paulinojr <paulino.junior@outlook.com>
2025-02-19 15:02:19 -0300

    feat: add default routes

--- beer-e-commerce-frontend/src/App.jsx
+++ beer-e-commerce-frontend/src/App.jsx
@@ -1,12 +1,21 @@
+//Routing
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import ProductListPage from './pages/ProductListPage'
 
+//Styles
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
+    <Router>
+      <Routes>
+        {/*Make /products the default route */}
+        <Route path="/" element={<Navigate to="/products" />} />
+
+        <Route path="products" element={<ProductListPage />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ beer-e-commerce-frontend/src/pages/ProductListPage.jsx
@@ -0,0 +1,9 @@
+function ProductListPage() {
+  return (
+    <>
+      <h1>Product List Page</h1>
+    </>
+  )
+}
+
+export default ProductListPage;
