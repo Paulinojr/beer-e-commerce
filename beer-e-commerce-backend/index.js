90e933e38839fd7c427f2cee4b2e3eb901282a42
Paulinojr <paulino.junior@outlook.com>
2025-02-20 20:37:36 -0300

    feat: implement stock-price route

--- beer-e-commerce-backend/index.js
+++ beer-e-commerce-backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import products from "./data/products.js"; 
+import stockPrice from "./data/stock-price.js";
+
 const app = express();
 const PORT = 8000;
 
@@ -10,6 +12,21 @@ app.get("/api/products", (req, res) => {
     res.json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/api/stock-price/:sku", (req, res) => {
+  try {
+    const sku = req.params.sku; 
+    const details = stockPrice[sku]; 
+    if (details) {
+      res.json(details);
+    } else {
+      res.status(404).json({ message: "SKU not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching stock price:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
