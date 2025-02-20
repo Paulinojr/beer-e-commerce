2c593b9fd0c1778180b56c6a84e63529cc06756a
Paulinojr <paulino.junior@outlook.com>
2025-02-20 19:46:19 -0300

    Feat: Proxy frontend calls to local backend

--- beer-e-commerce-frontend/vite.config.js
+++ beer-e-commerce-frontend/vite.config.js
@@ -6,9 +6,21 @@ import path from 'path'; // Import the path module
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   resolve: {
+  resolve: {
     alias: {
-      '@': path.resolve(path.dirname(new URL(import.meta.url).pathname), './src'),
+      "@": path.resolve(
+        path.dirname(new URL(import.meta.url).pathname),
+        "./src"
+      ),
     },
   },
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
